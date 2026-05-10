2e2387f4bdd9b4d097da88fa63827f6ae21f6930
Anders Bøgh Bruun <anders@boghbruun.dk>
2026-05-10 08:56:11 +0200

    feat(slumber): add package v5.2.5

---
+++ packages/slumber/default.nix
@@ -0,0 +1,30 @@
+{
+  lib,
+  fetchFromGitHub,
+  rustPlatform,
+}:
+
+rustPlatform.buildRustPackage (finalAttrs: {
+  pname = "slumber";
+  version = "5.2.5";
+
+  src = fetchFromGitHub {
+    owner = "LucasPickering";
+    repo = "slumber";
+    tag = "v${finalAttrs.version}";
+    hash = "sha256-6qdhBaX/YfRs5TWAjBxkTauBkX+8ppU+Xh6nYEMG7IE=";
+  };
+
+  cargoHash = "sha256-HiCyyvphFjYhuqXPa13Gq6QnxzQQ0KUy1S6w60dciXc=";
+
+  passthru.updateScript = ./update.sh;
+
+  meta = {
+    description = "Terminal-based HTTP/REST client";
+    homepage = "https://slumber.lucaspickering.me";
+    changelog = "https://github.com/LucasPickering/slumber/blob/v${finalAttrs.version}/CHANGELOG.md";
+    license = lib.licenses.mit;
+    mainProgram = "slumber";
+    maintainers = with lib.maintainers; [ javaes ];
+  };
+})
---
+++ packages/slumber/update.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+set -euo pipefail
+
+nix run github:Mic92/nix-update/1.13.1 -- \
+  --flake --override-filename packages/slumber/default.nix slumber
